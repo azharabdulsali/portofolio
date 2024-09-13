5010854fc1de5c25c2926fa15c86316ec0eb1c23
pickyfeekz <syafiq.azhar02@gmail.com>
2024-09-13 09:46:28 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/portofolio-details', function () {
+    return view('portfolio-details');
+});
+
+Route::get('/service-details', function () {
+    return view('service-details');
+});
+
+Route::get('/starter-page', function () {
+    return view('starter-page');
+});
